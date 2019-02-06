85d53b9c87caf0dbea956b70602d90972df2633b
finntroll_alex <frodo8787@mail.ru>
2019-02-06 23:34:42 +0300

    Частичные представления

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -8,48 +8,10 @@ using WebStore.Models;
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
-    {
-        private readonly List<EmployeeView> _employees = new List<EmployeeView>
-        {
-            new EmployeeView
-            {
-                Id = 1,
-                FirstName = "Семён",
-                SecondName = "Семёнов",
-                Patronymic = "Семёнович",
-                Age = 10
-            },
-            new EmployeeView
-            {
-                Id = 2,
-                FirstName = "Валерий",
-                SecondName = "Валерьев",
-                Patronymic = "Валерьевич",
-                Age = 20
-            },
-            new EmployeeView
-            {
-                Id = 3,
-                FirstName = "Фёдор",
-                SecondName = "Фёдоров",
-                Patronymic = "Фёдорович",
-                Age = 30
-            }
-        };
-
+    {      
         public IActionResult Index()
         {
-            return View(_employees);
-        }
-
-        public IActionResult Details(int id)
-        {
-            foreach (var item in _employees)
-            {
-                if (item.Id == id)
-                    return View(item);
-            }
-            return View("Index");
-        }
+            return View();
+        }               
     }
 }
