bfa3b5a0775be94b21adbd8ca42c89a181ede22c
finntroll_alex <frodo8787@mail.ru>
2019-02-15 01:27:50 +0300

    Победил оболочку, но далее не успел сделать. Остановился на DbInitializer

--- WebStore.DomainEntities/Entities/Product.cs
+++ WebStore.DomainEntities/Entities/Product.cs
@@ -37,6 +37,7 @@ namespace WebStore.DomainEntities.Entities
         /// <summary>
         /// Цена
         /// </summary>
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
