652b3d021739a19855db94d1dca44db20c2877f4
finntroll_alex <frodo8787@mail.ru>
2019-03-11 22:36:04 +0300

    написан UserController

---
+++ Common/WebStore.DomainEntities/DTO/User/AddClaimsDTO.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+
+namespace WebStore.DomainEntities.DTO.User
+{
+
+    public abstract class IdentityModelDTO
+    {
+        public Entities.User User { get; }
+    }
+
+    public class AddClaimsDTO : IdentityModelDTO
+    {
+        public IEnumerable<Claim> Claims { get; set; }
+    }
+
+    public class RemoveClaimsDTO : IdentityModelDTO
+    {
+        public IEnumerable<Claim> Claims { get; set; }
+    }
+
+    public class ReplaceClaimDTO : IdentityModelDTO
+    {
+        public Claim OldClaim { get; set;}
+        public Claim NewClaim { get; set; }
+    }
+
+    public class AddLoginDTO : IdentityModelDTO
+    {
+        public UserLoginInfo UserLoginInfo { get; set; }
+    }
+
+    public class PasswordHashDTO : IdentityModelDTO
+    {
+        public string Hash { get; set; }
+    }
+
+    public class SetLockoutDTO : IdentityModelDTO
+    {
+        public DateTimeOffset? LockOutEnd { get; set; }
+    }
+}
